16a37e37e31b795203c0f50fe066ef36625fcbb1
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-10 02:03:41 +0000

    Empieza a cargar cosas en el grafo

---
+++ TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/util/test/CargadorDeGrafosTest.java
@@ -0,0 +1,37 @@
+package ar.fi.uba.tda.util.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ar.fi.uba.tda.colecciones.Grafo;
+import ar.fi.uba.tda.util.CargadorDeGrafos;
+
+public class CargadorDeGrafosTest {
+
+	@Test
+	public void agregaUnaAristaUniendoDosVerticesDados() {
+		
+		Grafo grafo = new Grafo();
+		CargadorDeGrafos cargador = new CargadorDeGrafos(grafo);
+		cargador.agregarArista("vertice1", "vertice2");
+		
+		assertThat("Hay dos vertices", grafo.getCantidadDeNodosGrafo(), is(2));
+		assertThat("Hay un arco entre Vertice Uno y Dos", grafo.getVertices().primero().getAdyacentes().primero(), is(grafo.getVertices().ultimo()));
+	}
+	
+	@Test
+	@Ignore
+	public void noAgregaVerticesRepetidos() {
+		
+		Grafo grafo = new Grafo();
+		CargadorDeGrafos cargador = new CargadorDeGrafos(grafo);
+		cargador.agregarArista("vertice1", "vertice2");
+		cargador.agregarArista("vertice1", "vertice3");
+		
+		assertThat("Hay dos vertices", grafo.getCantidadDeNodosGrafo(), is(3));
+		assertThat("Hay un arco entre Vertice Uno y Dos", grafo.getVertices().primero().getAdyacentes().primero(), is(grafo.getVertices().ultimo()));
+	}
+}
