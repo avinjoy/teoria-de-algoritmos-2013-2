cea273cae6dcf1747505c7c7463130c2560f8b65
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-10 05:03:51 +0000

    Empiezo a leer el grafo desde el archivo

--- trunk/TeoriaDeAlgoritmos/src/main/java/ar/fi/uba/tda/util/CargadorDeGrafos.java
+++ trunk/TeoriaDeAlgoritmos/src/main/java/ar/fi/uba/tda/util/CargadorDeGrafos.java
@@ -1,25 +1,48 @@
 package ar.fi.uba.tda.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import ar.fi.uba.tda.colecciones.Grafo;
 import ar.fi.uba.tda.colecciones.Vertice;
 
 public class CargadorDeGrafos {
 
-	private final Grafo grafo;
+	private final Grafo<String> grafo;
 
-	public CargadorDeGrafos(Grafo grafo) {
+	public CargadorDeGrafos(Grafo<String> grafo) {
 		this.grafo = grafo;
 		
 	}
 
 	public void agregarArista(String verticeInicial, String verticeFinal) {
 		
-		Vertice inicio = new Vertice(verticeInicial);
+		Vertice<String> inicio = new Vertice<String>(verticeInicial);
 		
-		Vertice fin = new Vertice(verticeFinal);
+		Vertice<String> fin = new Vertice<String>(verticeFinal);
 		
 		grafo.agregarArco(inicio, fin);
 		
+	}
+
+	public void cargar(BufferedReader reader) throws IOException {
+		
+		String linea = reader.readLine();
+		
+		while(linea != null) {
+			
+			String[] vertices = linea.split(":");
+			
+			String vertice = vertices[0];
+			String[] adyacentes = vertices[1].split(",");
+			
+			for (int i = 0; i < adyacentes.length; i++) {
+				
+				agregarArista(vertice, adyacentes[i]);
+			}
+			
+			linea = reader.readLine();
+		}
 	}
 
 }
--- trunk/TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/util/test/CargadorDeGrafosTest.java
+++ trunk/TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/util/test/CargadorDeGrafosTest.java
@@ -3,7 +3,13 @@ package ar.fi.uba.tda.util.test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 import ar.fi.uba.tda.colecciones.Grafo;
 import ar.fi.uba.tda.colecciones.Vertice;
@@ -11,11 +17,19 @@ import ar.fi.uba.tda.util.CargadorDeGrafos;
 
 public class CargadorDeGrafosTest {
 
+	private Grafo<String> grafo;
+	private CargadorDeGrafos cargador;
+
+	@Before
+	public void setup() {
+	
+		grafo = new Grafo<String>();
+		cargador = new CargadorDeGrafos(grafo);
+	}
+	
 	@Test
 	public void agregaUnaAristaUniendoDosVerticesDados() {
 		
-		Grafo<String> grafo = new Grafo<String>();
-		CargadorDeGrafos cargador = new CargadorDeGrafos(grafo);
 		cargador.agregarArista("vertice1", "vertice2");
 		
 		assertThat("la cantidad de vertices", grafo.getCantidadDeNodosGrafo(), is(2));
@@ -25,12 +39,51 @@ public class CargadorDeGrafosTest {
 	@Test
 	public void noAgregaVerticesRepetidos() {
 		
-		Grafo<String> grafo = new Grafo<String>();
-		CargadorDeGrafos cargador = new CargadorDeGrafos(grafo);
 		cargador.agregarArista("vertice1", "vertice2");
 		cargador.agregarArista("vertice1", "vertice3");
 		
 		assertThat("la cantidad de vertices", grafo.getCantidadDeNodosGrafo(), is(3));
 		assertThat("el vertice uno y el dos", grafo.getVertices().primero().getAdyacentes().primero(), is(new Vertice<String>("vertice2")));
+	}
+	
+	@Test
+	public void cargaDesdeUnArchivoUnVerticeConUnAdyacente() throws IOException {
+		
+		BufferedReader reader = mock(BufferedReader.class);
+		when(reader.readLine()).thenReturn("A:B", (String)null);
+		
+		cargador.cargar(reader);
+		
+		Vertice<String> verticeA = new Vertice<String>("A");
+		Vertice<String> verticeB = new Vertice<String>("B");
+		
+		assertThat("la cantidad de vertices", grafo.getCantidadDeNodosGrafo(), is(2));
+		
+		assertThat("el primer vertice", grafo.getVertices().primero(), is(verticeA));
+		assertThat("el segundo vertice", grafo.getVertices().ultimo(), is(verticeB));
+		
+		assertThat("el vertice A y el B", grafo.getVertices().primero().getAdyacentes().primero(), is(verticeB));
+	}
+	
+	@Test
+	@Ignore("Hay que modificar el grafo para que busque el vértice a insertar y devuelva ese")
+	public void cargaDesdeUnArchivoUnVerticeConDosAdyacentes() throws IOException {
+		
+		BufferedReader reader = mock(BufferedReader.class);
+		when(reader.readLine()).thenReturn("A:B,C", (String)null);
+		
+		cargador.cargar(reader);
+		
+		Vertice<String> verticeA = new Vertice<String>("A");
+		Vertice<String> verticeB = new Vertice<String>("B");
+		Vertice<String> verticeC = new Vertice<String>("C");
+		
+		assertThat("la cantidad de vertices", grafo.getCantidadDeNodosGrafo(), is(3));
+		
+		assertThat("el primer vertice", grafo.getVertices().primero(), is(verticeA));
+		assertThat("el tercer vertice", grafo.getVertices().ultimo(), is(verticeC));
+		
+		assertThat("el vertice A y el B", grafo.getVertices().primero().getAdyacentes().primero(), is(verticeB));
+		assertThat("el vertice A y el C", grafo.getVertices().primero().getAdyacentes().ultimo(), is(verticeC));
 	}
 }
