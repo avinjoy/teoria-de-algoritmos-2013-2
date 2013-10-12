8f3f2fee7c2e597fdf32ed2bd1e830e8449b7ae6
julian20dambrosio@gmail.com <julian20dambrosio@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-12 19:09:36 +0000

    V1 COMPILA ! JDAMBROSIO

---
+++ Robustez/Robustez/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robustez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Robustez/Robustez/Vertice.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeoriaDelAlgoritmosCSHARP
+{
+    class Vertice <T>
+    {
+        private T contenido; //TODO:Debemos ver que tipo de contenido ubicar ac�
+
+        public T Contenido
+        {
+            get { return contenido; }
+            set { contenido = value; }
+        }
+        private bool visitado;
+
+        public bool Visitado
+        {
+            get { return visitado; }
+            set { visitado = value; }
+        }
+        private ListaEnlazada<Vertice<T>> adyacentes;
+
+        public ListaEnlazada<Vertice<T>> Adyacentes
+        {
+            get { return adyacentes; }
+            set { adyacentes = value; }
+        }
+		
+	    public Vertice(): base() {
+            
+	    }
+	
+	    public Vertice(T contenido): base(){
+		    this.contenido=contenido;
+		    this.adyacentes=new ListaEnlazada<Vertice<T>>();
+	    }
+	
+	    public T getContenido() {
+		    return contenido;
+	    }
+	    public void setContenido(T contenido) {
+		    this.contenido = contenido;
+	    }
+	    public ListaEnlazada<Vertice<T>> getAdyacentes() {
+		    return adyacentes;
+	    }
+	    public void setAdyacentes(ListaEnlazada<Vertice<T>> adyacentes) {
+		    this.adyacentes = adyacentes;
+	    }
+
+	    public bool isVisitado() {
+		    return visitado;
+	    }
+
+	    public void setVisitado(bool vistado) {
+		    this.visitado = vistado;
+	    }
+	
+	    public Int32 getGradoVertice(){
+            return this.adyacentes.GetTamanio();
+	    }
+
+	    public int hashCode() {
+		    const int prime = 31;
+		    int result = 1;
+		    result = prime * result
+				    + ((contenido == null) ? 0 : contenido.GetHashCode());
+		    return result;
+	    }
+
+	    public bool equals(Object obj) 
+        {
+		    if (this == obj) {
+			    return true;
+		    }
+		    if (obj == null) {
+			    return false;
+		    }
+		    if (!(obj is Vertice<T>)) {
+			    return false;
+		    }
+		    Vertice<T> other = (Vertice<T>) obj;
+		    if (contenido == null) {
+			    if (other.contenido != null) {
+				    return false;
+			    }
+		    } else if (!contenido.Equals(other.contenido)) {
+			    return false;
+		    }
+		    return true;
+	    }
+    }
+}
