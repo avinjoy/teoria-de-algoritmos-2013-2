cd899c9300d70def83f3e9a848c7591a1bec21ab
julian20dambrosio@gmail.com <julian20dambrosio@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-12 19:09:36 +0000

    V1 COMPILA ! JDAMBROSIO

---
+++ trunk/Robustez/Robustez/Grafo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeoriaDelAlgoritmosCSHARP
+{
+    class Grafo <T>
+    {
+        private ListaEnlazada<Vertice<T>> vertices;
+
+
+        public Grafo(ListaEnlazada<Vertice<T>> vertices): base()
+        {
+            this.vertices = vertices;
+        }
+
+        public Grafo()
+        {
+            this.vertices = new ListaEnlazada<Vertice<T>>();
+        }
+
+        public ListaEnlazada<Vertice<T>> getVertices()
+        {
+            return vertices;
+        }
+
+        public void setVertices(ListaEnlazada<Vertice<T>> vertices)
+        {
+            this.vertices = vertices;
+        }
+
+        public Int32 getCantidadDeNodosGrafo()
+        {
+            return this.vertices.GetTamanio();
+        }
+
+        /**
+         * Recorrido en profundidad
+         */
+        public void recorridoDFS()
+        {
+            return;
+        }
+
+
+
+        /**
+         * Recorrido en ancho
+         */
+        public void recorridoBFS()
+        {
+            return;
+
+        }
+
+        /**
+         * Agrega un v�rtice al grafo
+         * @param vert
+         */
+        public void agregarVertice(Vertice<T> vert)
+        {
+            if (vert != null && vert.getContenido() != null && !this.contieneVertice(vert))
+            {
+                this.vertices.agregar(vert);
+            }
+
+        }
+
+        /**
+         * Crea un arco entre 2 v�rtices (no es grafo dirigido)
+         * @param inicio
+         * @param fin
+         */
+        public void agregarArco(Vertice<T> inicio, Vertice<T> fin)
+        {
+            //TODO:Validaciones!!!	
+            inicio.getAdyacentes().agregar(fin);
+            fin.getAdyacentes().agregar(inicio);
+
+            this.agregarVertice(inicio);
+            this.agregarVertice(fin);
+
+        }
+
+        public bool contieneVertice(Vertice<T> verticeBuscado)
+        {
+
+            return vertices.contiene(verticeBuscado);
+        }
+    }
+}
---
+++ trunk/Robustez/Robustez/ListaEnlazada.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeoriaDelAlgoritmosCSHARP
+{
+    public class ListaEnlazada<T>
+    {
+        private static Elemento<T> header = new Elemento<T>(default(T), null, null);
+        private static Int32 tamanio = 0;
+
+        public static Int32 Tamanio
+        {
+            get { return ListaEnlazada < T >.tamanio; }
+            set { ListaEnlazada<T>.tamanio = value; }
+        }
+
+	    public ListaEnlazada () {
+
+		    header.Siguiente = header.Anterior = header;
+	    }
+	
+	    public bool vacia() {
+		    return true;
+	    }
+
+	    public T primero() {
+            return header.Siguiente.Vertice;
+	    }
+
+	    public T ultimo() {
+		    return header.Anterior.Vertice;
+	    }
+	
+	    public void agregar(T elemento) {
+		    agregarAntes(elemento, header);
+	    }
+        public Int32 GetTamanio()
+        {
+            return tamanio;
+        }
+	
+	    private void agregarAntes(T elemento, Elemento<T> siguiente) {
+		
+		    Elemento<T> nuevo = new Elemento<T>(elemento, siguiente, siguiente.Anterior);
+		    nuevo.Anterior.Siguiente = nuevo;
+            nuevo.Siguiente.Anterior = nuevo;
+		    tamanio++;
+	    }
+	
+	  
+
+	    public IEnumerable<T> iterador() {
+		    return new IteradorListaEnlazada();
+	    }
+	
+	    private class IteradorListaEnlazada : IEnumerable<T> {
+
+		    private int siguiente;
+		    private Elemento<T> siguienteElemento;
+		
+		    public IteradorListaEnlazada () {
+		
+			    siguiente = 0;
+			    siguienteElemento = header;
+		    }
+		
+
+		    public bool hasNext() {
+			    return siguiente != Tamanio;
+		    }
+
+		    public T next() {
+			
+			    T aRetornar = default(T);
+			
+			    if (siguiente < Tamanio) {
+				
+				    aRetornar = siguienteElemento.Siguiente.Vertice;
+				    siguienteElemento = siguienteElemento.Siguiente;
+				    siguiente++;
+			    }
+			
+			    return aRetornar;
+		    }
+
+
+		    public void remove() {
+			
+		    }
+
+
+            #region IEnumerable<T> Members
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                throw new Exception("The method or operation is not implemented.");
+            }
+
+            #endregion
+
+            #region IEnumerable Members
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                throw new Exception("The method or operation is not implemented.");
+            }
+
+            #endregion
+        }
+	
+	    private class Elemento<T> {
+
+            private T vertice;
+            private Elemento<T> anterior;
+            private Elemento<T> siguiente;
+
+            public T Vertice
+            {
+                get { return vertice; }
+                set { vertice = value; }
+            }
+            
+            public Elemento<T> Anterior
+            {
+                get { return anterior; }
+                set { anterior = value; }
+            }
+            
+
+            public Elemento<T> Siguiente
+            {
+                get { return siguiente; }
+                set { siguiente = value; }
+            }
+		
+		    public Elemento(T elemento, Elemento<T> siguiente, Elemento<T> anterior) {
+			
+			    this.vertice = elemento;
+			    this.anterior = anterior;
+			    this.siguiente = siguiente;
+		    }
+
+	    }
+
+	    public bool contiene(T datoBuscado) {
+		
+		    bool encontrado = false;
+		    IEnumerable<T> iterador = this.iterador();
+		
+		    while (!encontrado && iterador.GetEnumerator().MoveNext()) {
+			
+			    T item = iterador.GetEnumerator().Current;
+			
+			    if (item.Equals(datoBuscado)) {
+				    encontrado = true;
+			    }
+		    }
+		
+		    return encontrado;
+	    }
+
+       
+    }
+}
