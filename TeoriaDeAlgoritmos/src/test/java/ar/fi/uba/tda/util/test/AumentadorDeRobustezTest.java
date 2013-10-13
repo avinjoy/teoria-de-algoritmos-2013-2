1b36ec89adff199a69b335f64dfba83f386a49a5
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-13 20:25:18 +0000

    Control para no agregar más aristas que la robustez deseada

--- TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/util/test/AumentadorDeRobustezTest.java
+++ TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/util/test/AumentadorDeRobustezTest.java
@@ -137,4 +137,96 @@ public class AumentadorDeRobustezTest {
 		
 		MatcherAssert.assertThat("la cantidad de aristas agregadas", aristasAgregadas.tamanio(), Matchers.is(0));
 	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void siRecibeDosGruposDeDosNodosYRobustez2AgregaDosAristas() {
+		
+		Grafo<String> grafo = new Grafo<String>();
+		Vertice<String> verticeA = new Vertice<String>("A");
+		Vertice<String> verticeB = new Vertice<String>("B");
+		Vertice<String> verticeC = new Vertice<String>("C");
+		Vertice<String> verticeD = new Vertice<String>("D");
+		
+		grafo.agregarVertice(verticeA);
+		grafo.agregarVertice(verticeB);
+		grafo.agregarVertice(verticeC);
+		grafo.agregarVertice(verticeD);
+		
+		grafo.agregarArco(verticeA, verticeB);
+		grafo.agregarArco(verticeC, verticeD);
+		
+		AumentadorDeRobustez aumentador = new AumentadorDeRobustez(grafo);
+		
+		ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos = new ListaEnlazada<ListaEnlazada<Vertice<?>>>();
+		
+		ListaEnlazada<Vertice<?>> primerCiclo = new ListaEnlazada<Vertice<?>>();
+		primerCiclo.agregar(verticeA);
+		primerCiclo.agregar(verticeB);
+		
+		ListaEnlazada<Vertice<?>> segundoCiclo = new ListaEnlazada<Vertice<?>>();
+		segundoCiclo.agregar(verticeC);
+		segundoCiclo.agregar(verticeD);
+		
+		ciclos.agregar(primerCiclo);
+		ciclos.agregar(segundoCiclo);
+		
+		int robustez = 2;
+		
+		aumentador.aumentar(ciclos, robustez);
+		
+		ListaEnlazada<Arista> aristasAgregadas = aumentador.getAristasAgregadas();
+		
+		MatcherAssert.assertThat("la cantidad de aristas agregadas", aristasAgregadas.tamanio(), Matchers.is(2));
+		MatcherAssert.assertThat("el origen de la primera arista", (Vertice<String>) aristasAgregadas.primero().getOrigen(), Matchers.is(verticeA));
+		MatcherAssert.assertThat("el destino de la primera arista", (Vertice<String>) aristasAgregadas.primero().getDestino(), Matchers.is(verticeC));
+		
+		MatcherAssert.assertThat("el origen de la segudna arista", (Vertice<String>) aristasAgregadas.ultimo().getOrigen(), Matchers.is(verticeB));
+		MatcherAssert.assertThat("el destino de la segunda arista", (Vertice<String>) aristasAgregadas.ultimo().getDestino(), Matchers.is(verticeD));
+	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void siRecibeDosGruposDeDosNodosYRobustez1AgregaUnaArista() {
+		
+		Grafo<String> grafo = new Grafo<String>();
+		Vertice<String> verticeA = new Vertice<String>("A");
+		Vertice<String> verticeB = new Vertice<String>("B");
+		Vertice<String> verticeC = new Vertice<String>("C");
+		Vertice<String> verticeD = new Vertice<String>("D");
+		
+		grafo.agregarVertice(verticeA);
+		grafo.agregarVertice(verticeB);
+		grafo.agregarVertice(verticeC);
+		grafo.agregarVertice(verticeD);
+		
+		grafo.agregarArco(verticeA, verticeB);
+		grafo.agregarArco(verticeC, verticeD);
+		
+		AumentadorDeRobustez aumentador = new AumentadorDeRobustez(grafo);
+		
+		ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos = new ListaEnlazada<ListaEnlazada<Vertice<?>>>();
+		
+		ListaEnlazada<Vertice<?>> primerCiclo = new ListaEnlazada<Vertice<?>>();
+		primerCiclo.agregar(verticeA);
+		primerCiclo.agregar(verticeB);
+		
+		ListaEnlazada<Vertice<?>> segundoCiclo = new ListaEnlazada<Vertice<?>>();
+		segundoCiclo.agregar(verticeC);
+		segundoCiclo.agregar(verticeD);
+		
+		ciclos.agregar(primerCiclo);
+		ciclos.agregar(segundoCiclo);
+		
+		int robustez = 1;
+		
+		aumentador.aumentar(ciclos, robustez);
+		
+		ListaEnlazada<Arista> aristasAgregadas = aumentador.getAristasAgregadas();
+		
+		MatcherAssert.assertThat("la cantidad de aristas agregadas", aristasAgregadas.tamanio(), Matchers.is(1));
+		MatcherAssert.assertThat("el origen de la primera arista", (Vertice<String>) aristasAgregadas.primero().getOrigen(), Matchers.is(verticeA));
+		MatcherAssert.assertThat("el destino de la primera arista", (Vertice<String>) aristasAgregadas.primero().getDestino(), Matchers.is(verticeC));
+		
+	}
 }
