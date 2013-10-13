74648fe0e7df96cb1f979a085b85052e1c515e16
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-13 00:25:23 +0000

    Imports no usados eliminados

--- trunk/TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/colecciones/test/GrafoTest.java
+++ trunk/TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/colecciones/test/GrafoTest.java
@@ -3,7 +3,6 @@ package ar.fi.uba.tda.colecciones.test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import org.hamcrest.core.IsEqual;
 import org.junit.Before;
 import org.junit.Test;
 
