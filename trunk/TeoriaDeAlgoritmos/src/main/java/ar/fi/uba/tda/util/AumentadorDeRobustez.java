358c190177c6f541a99bfaf3a5d9c6651e37e82d
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-10-13 01:23:35 +0000

    Principio del algoritmo para aumentar la robustez.

---
+++ trunk/TeoriaDeAlgoritmos/src/main/java/ar/fi/uba/tda/util/AumentadorDeRobustez.java
@@ -0,0 +1,68 @@
+package ar.fi.uba.tda.util;
+
+import java.util.Iterator;
+
+import ar.fi.uba.tda.colecciones.Arista;
+import ar.fi.uba.tda.colecciones.Grafo;
+import ar.fi.uba.tda.colecciones.ListaEnlazada;
+import ar.fi.uba.tda.colecciones.Vertice;
+
+public class AumentadorDeRobustez {
+
+	private final Grafo<?> grafo;
+	private ListaEnlazada<Arista> aristasAgregadas = new ListaEnlazada<Arista>();
+
+	public AumentadorDeRobustez(Grafo<?> grafo) {
+		this.grafo = grafo;
+	}
+
+	public void aumentar(ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos, int robustez) {
+
+		if (laRobustezEsCompatibleConElGrafo(robustez)) {
+			
+			Iterator<ListaEnlazada<Vertice<?>>> listaDeCiclos = ciclos.iterador();
+			
+			while (listaDeCiclos.hasNext()) {
+				
+				ListaEnlazada<Vertice<?>> ciclo = listaDeCiclos.next();
+				
+				ListaEnlazada<Vertice<?>> siguienteCiclo = obtenerSiguienteCiclo(ciclos, listaDeCiclos);
+				
+				Iterator<Vertice<?>> verticesPrimerCiclo = ciclo.iterador();
+				Iterator<Vertice<?>> verticesSegundoCiclo = siguienteCiclo.iterador();
+				
+				while (verticesPrimerCiclo.hasNext() && verticesSegundoCiclo.hasNext()) {
+					
+					Vertice<?> verticeCicloUno = verticesPrimerCiclo.next();
+					Vertice<?> verticeCicloDos = verticesSegundoCiclo.next();
+					
+					aristasAgregadas.agregar(new Arista(verticeCicloUno, verticeCicloDos));
+				}
+				
+			}
+		}
+	}
+
+	private boolean laRobustezEsCompatibleConElGrafo(int robustez) {
+		return grafo.getCantidadDeNodosGrafo() > robustez;
+	}
+
+	private ListaEnlazada<Vertice<?>> obtenerSiguienteCiclo(ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos,
+															Iterator<ListaEnlazada<Vertice<?>>> listaDeCiclos) {
+		
+		ListaEnlazada<Vertice<?>> siguienteCiclo;
+		
+		if (listaDeCiclos.hasNext()) {
+			siguienteCiclo = listaDeCiclos.next();
+		} else {
+			siguienteCiclo = ciclos.primero();
+		}
+		
+		return siguienteCiclo;
+	}
+
+	public ListaEnlazada<Arista> getAristasAgregadas() {
+		return aristasAgregadas;
+	}
+
+}
---
+++ trunk/TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/util/test/AumentadorDeRobustezTest.java
@@ -0,0 +1,105 @@
+package ar.fi.uba.tda.util.test;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import ar.fi.uba.tda.colecciones.Arista;
+import ar.fi.uba.tda.colecciones.Grafo;
+import ar.fi.uba.tda.colecciones.ListaEnlazada;
+import ar.fi.uba.tda.colecciones.Vertice;
+import ar.fi.uba.tda.util.AumentadorDeRobustez;
+
+public class AumentadorDeRobustezTest {
+
+	@Test
+	public void siRecibeUnNodoYRobustez1NoAgregaAristas() {
+		
+		Grafo<String> grafo = new Grafo<String>();
+		
+		AumentadorDeRobustez aumentador = new AumentadorDeRobustez(grafo);
+		
+		ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos = new ListaEnlazada<ListaEnlazada<Vertice<?>>>();
+		
+		ListaEnlazada<Vertice<?>> primerCiclo = new ListaEnlazada<Vertice<?>>();
+		
+		primerCiclo.agregar(new Vertice<String>());
+		ciclos.agregar(primerCiclo);
+		int robustez = 1;
+		
+		aumentador.aumentar(ciclos, robustez);
+		
+		ListaEnlazada<Arista> aristasAgregadas = aumentador.getAristasAgregadas();
+		
+		MatcherAssert.assertThat("la cantidad de aristas agregadas", aristasAgregadas.tamanio(), Matchers.is(0));
+	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void siRecibeDosNodosYRobustez1AgregaUnaArista() {
+		
+		Grafo<String> grafo = new Grafo<String>();
+		Vertice<String> verticeA = new Vertice<String>("A");
+		Vertice<String> verticeB = new Vertice<String>("B");
+		
+		grafo.agregarVertice(verticeA);
+		grafo.agregarVertice(verticeB);
+		
+		AumentadorDeRobustez aumentador = new AumentadorDeRobustez(grafo);
+		
+		ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos = new ListaEnlazada<ListaEnlazada<Vertice<?>>>();
+		
+		ListaEnlazada<Vertice<?>> primerCiclo = new ListaEnlazada<Vertice<?>>();
+		primerCiclo.agregar(verticeA);
+		
+		ListaEnlazada<Vertice<?>> segundoCiclo = new ListaEnlazada<Vertice<?>>();
+		segundoCiclo.agregar(verticeB);
+		
+		
+		ciclos.agregar(primerCiclo);
+		ciclos.agregar(segundoCiclo);
+		
+		int robustez = 1;
+		
+		aumentador.aumentar(ciclos, robustez);
+		
+		ListaEnlazada<Arista> aristasAgregadas = aumentador.getAristasAgregadas();
+		
+		MatcherAssert.assertThat("la cantidad de aristas agregadas", aristasAgregadas.tamanio(), Matchers.is(1));
+		MatcherAssert.assertThat("el origen de la arista", (Vertice<String>) aristasAgregadas.primero().getOrigen(), Matchers.is(verticeA));
+		MatcherAssert.assertThat("el destino de la arista", (Vertice<String>) aristasAgregadas.primero().getDestino(), Matchers.is(verticeB));
+	}
+	
+	@Test
+	public void siRecibeDosNodosYRobustez2NoAgregaArista() {
+		
+		Grafo<String> grafo = new Grafo<String>();
+		Vertice<String> verticeA = new Vertice<String>("A");
+		Vertice<String> verticeB = new Vertice<String>("B");
+		
+		grafo.agregarVertice(verticeA);
+		grafo.agregarVertice(verticeB);
+		
+		AumentadorDeRobustez aumentador = new AumentadorDeRobustez(grafo);
+		
+		ListaEnlazada<ListaEnlazada<Vertice<?>>> ciclos = new ListaEnlazada<ListaEnlazada<Vertice<?>>>();
+		
+		ListaEnlazada<Vertice<?>> primerCiclo = new ListaEnlazada<Vertice<?>>();
+		primerCiclo.agregar(verticeA);
+		
+		ListaEnlazada<Vertice<?>> segundoCiclo = new ListaEnlazada<Vertice<?>>();
+		segundoCiclo.agregar(verticeB);
+		
+		
+		ciclos.agregar(primerCiclo);
+		ciclos.agregar(segundoCiclo);
+		
+		int robustez = 2;
+		
+		aumentador.aumentar(ciclos, robustez);
+		
+		ListaEnlazada<Arista> aristasAgregadas = aumentador.getAristasAgregadas();
+		
+		MatcherAssert.assertThat("la cantidad de aristas agregadas", aristasAgregadas.tamanio(), Matchers.is(0));
+	}
+}
