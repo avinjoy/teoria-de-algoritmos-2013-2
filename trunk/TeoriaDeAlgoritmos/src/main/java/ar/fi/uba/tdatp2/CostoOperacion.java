915c956a7e6e637dc538532f1431cfffc56763db
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-11-06 20:39:25 +0000

    Emprolijo un poco el código

--- trunk/TeoriaDeAlgoritmos/src/main/java/ar/fi/uba/tdatp2/CostoOperacion.java
+++ trunk/TeoriaDeAlgoritmos/src/main/java/ar/fi/uba/tdatp2/CostoOperacion.java
@@ -1,6 +1,5 @@
 package ar.fi.uba.tdatp2;
 
-
 public class CostoOperacion {
 
 	public enum TipoOperacion{
@@ -98,6 +97,7 @@ public class CostoOperacion {
 				return j;
 			}
 		},
+
 		INTERCAMBIAR(1) {
 			@Override
 			public void ejecutar(String palabraInicio, String palabraFin, StringBuilder palabraResultante, int i, int j) {
@@ -124,6 +124,7 @@ public class CostoOperacion {
 		private TipoOperacion(int costo) {
 			this.costo = costo;
 		}
+		
 		public abstract void ejecutar(String palabraInicio, String palabraFin, StringBuilder palabraResultante, int i, int j);
 
 		public abstract int nuevoI(int i);
@@ -143,7 +144,13 @@ public class CostoOperacion {
 	private int filaAnterior;
 	private int colAnterior;
 	private TipoOperacion op;
-	private char letter;
+	
+	public CostoOperacion(int filaAnterior, int colAnterior, int costo,TipoOperacion op) {
+		this.costo = costo;
+		this.filaAnterior = filaAnterior;
+		this.colAnterior = colAnterior;
+		this.op = op;
+	}
 	
 	public int getCosto() {
 		return costo;
@@ -169,38 +176,11 @@ public class CostoOperacion {
 	public void setOp(TipoOperacion op) {
 		this.op = op;
 	}
-	
-	public char getLetter() {
-		return letter;
-	}
-	public void setLetter(char letter) {
-		this.letter = letter;
-	}
-	
-	public CostoOperacion(int filaAnterior, int colAnterior, int costo,
-			TipoOperacion op) {
-		super();
-		this.costo = costo;
-		this.filaAnterior = filaAnterior;
-		this.colAnterior = colAnterior;
-		this.op = op;
-	}
-	
-	public CostoOperacion(int costo, int filaAnterior, int colAnterior,
-			TipoOperacion op, char letter) {
-		super();
-		this.costo = costo;
-		this.filaAnterior = filaAnterior;
-		this.colAnterior = colAnterior;
-		this.letter = letter;
-		this.op = op;
-	}
-	
+
 	@Override
 	public String toString() {
 		return "CostoOperacion [costo=" + costo + ", filaAnterior="
-				+ filaAnterior + ", colAnterior=" + colAnterior + ", op=" + op
-				+ ", letter=" + letter+"]";
+				+ filaAnterior + ", colAnterior=" + colAnterior + ", op=" + op + "]";
 	}
 	
 	
