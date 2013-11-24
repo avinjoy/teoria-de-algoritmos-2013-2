6708b40ad682b8eed22e626b95ef546ea2590938
nrisaro@gmail.com <nrisaro@gmail.com@ec24d4be-5085-f638-ce10-2b6d3479cffe>
2013-11-24 20:18:19 +0000

    Elijo dependiendo de un parámetro de donde leer los datos

---
+++ trunk/TeoriaDeAlgoritmos/src/test/java/ar/fi/uba/tda/tdatp3/SelectorDeFuenteTest.java
@@ -0,0 +1,39 @@
+package ar.fi.uba.tda.tdatp3;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+import ar.fi.uba.tdatp3.CargadorDeElementos;
+import ar.fi.uba.tdatp3.FuenteDeDatos;
+import ar.fi.uba.tdatp3.GeneradorDeCasosRandom;
+
+public class SelectorDeFuenteTest {
+	
+	@Test
+	public void siElParametroTerminaEnTXTLaFuenteEsElArchivo() {
+		
+		FuenteDeDatos fuente = SelectorDeFuente.obtenerFuente("datos.txt");
+		
+		assertThat("la fuente", fuente, is(instanceOf(CargadorDeElementos.class)));
+	}
+	
+	@Test
+	public void siElParametroEmpiezaConGuionPLaFuenteEsRandom() {
+		
+		FuenteDeDatos fuente = SelectorDeFuente.obtenerFuente("-P1000");
+		
+		assertThat("la fuente", fuente, is(instanceOf(GeneradorDeCasosRandom.class)));
+	}
+	
+	@Test
+	public void siLaFuenteEsRandomElParametroIndicaLaCantidadDeElementosAObtener() {
+		
+		FuenteDeDatos fuente = SelectorDeFuente.obtenerFuente("-P1000");
+		
+		assertThat("la fuente", fuente.obtenerDatos(), hasSize(1000));
+		
+	}
+
+}
